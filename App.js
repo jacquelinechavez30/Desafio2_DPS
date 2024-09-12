27abcfbb97e33ba2df1727035c21d931148a858d
i-ashy <ashleymelinares@gmail.com>
2024-09-12 02:47:48 -0600

    storage no funcional

--- App.js
+++ App.js
@@ -7,17 +7,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Stackdatos from './src/screens/Stackdatos';
 import Productoofertas from './src/screens/Productoofertas';
 import { IngresosProvider } from './src/screens/IngresosContext';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-native-gesture-handler';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
 
-  const [datos, setDatos] = useState([]);
+  //const [datos, setDatos] = useState([]);
 
   //se ocupa para cargar los datos del storage
-  useEffect(() => {
+  /*useEffect(() => {
     const obtenerDatosStorage = async () => {
       try {
         const datosStorage = await AsyncStorage.getItem('datos');
@@ -29,17 +29,17 @@ export default function App() {
           }
         }
 
-        obtenerDatosStorage();}, []);
+        obtenerDatosStorage();}, []);*/
 
   // guardar los datos en storage
-  const guardarDatosStorage = async (datosJSON) => {
+  /*const guardarDatosStorage = async (datosJSON) => {
     try {
       await AsyncStorage.setItem('datos', datosJSON.stringify(nuevosDatos));
       setDatos(nuevosDatos);
     } catch (error) {
         console.log(error);
     }
-  }
+  }*/
   
   return (
 
